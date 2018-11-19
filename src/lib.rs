a85cdf0c7c9f245d7a5cbc54762716a889d3dcea
Sergey Shandar <sergey.shandar@gmail.com>
2018-11-18 21:54:09 -0800

    simplify a little bit.

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,46 @@
 
-pub struct Rev<T, I> 
-where 
-    T: IntoIterator<Item = I> + Copy,
-    T::IntoIter: DoubleEndedIterator, 
+pub struct Rev<T>
+where
+    T: IntoIterator + Copy,
+    T::IntoIter: DoubleEndedIterator,
 {
-    pub original: T
+    original: T
 }
 
-pub fn rev<T, I>(v: T) -> Rev<T, I> 
-where 
-    T: IntoIterator<Item = I> + Copy,
+impl<T> Rev<T>
+where
+    T: IntoIterator + Copy,
     T::IntoIter: DoubleEndedIterator
 {
-    Rev { original: v }
+    pub fn new(v: T) -> Self { Self { original: v } }
 }
 
-impl<T, I> IntoIterator for Rev<T, I> 
-where 
-    T: IntoIterator<Item = I> + Copy,
-    T::IntoIter: DoubleEndedIterator 
+impl<T> IntoIterator for Rev<T>
+where
+    T: IntoIterator + Copy,
+    T::IntoIter: DoubleEndedIterator
 {
-    type Item = I;
+    type Item = T::Item;
     type IntoIter = std::iter::Rev<T::IntoIter>;
     fn into_iter(self) -> Self::IntoIter {
         self.original.into_iter().rev()
     }
 }
 
-impl<T, I> Copy for Rev<T, I> 
+impl<T> Copy for Rev<T>
 where
-    T: IntoIterator<Item = I> + Copy,
-    T::IntoIter: DoubleEndedIterator 
+    T: IntoIterator + Copy,
+    T::IntoIter: DoubleEndedIterator
 {
 }
 
-impl<T, I> Clone for Rev<T, I> 
-where 
-    T: IntoIterator<Item = I> + Copy,
-    T::IntoIter: DoubleEndedIterator 
+impl<T> Clone for Rev<T>
+where
+    T: IntoIterator + Copy,
+    T::IntoIter: DoubleEndedIterator
 {
     fn clone(&self) -> Self {
-        rev(self.original)
+        Self::new(self.original)
     }
 }
 
@@ -50,7 +50,7 @@ mod tests {
     #[test]
     fn test() {
         let a = [1, 2, 3];
-        let x = rev(&a);
+        let x = Rev::new(&a);
         let d: Vec<i32> = x.into_iter().map(|&x| x).collect();
         assert_eq!(d, vec!(3, 2, 1));
     }
