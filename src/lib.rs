c8650996023587cde3710c01593f3839be93a854
Sergey Shandar <sergey.shandar@gmail.com>
2019-01-31 06:21:20 -0800

    0.4.1 default traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,31 @@
-pub struct RevIntoIter<T>
-    where
-        T: IntoIterator + Copy,
-        T::IntoIter: DoubleEndedIterator,
+pub struct RevIntoIter<T: IntoIterator + Copy>
+    where T::IntoIter: DoubleEndedIterator
 {
     original: T
 }
 
 pub trait Rev: IntoIterator + Copy
-    where Self::IntoIter: DoubleEndedIterator {
-    fn rev(self) -> RevIntoIter<Self>;
-}
-
-impl<T> Rev for T
-    where
-        T: IntoIterator + Copy,
-        T::IntoIter: DoubleEndedIterator,
+    where Self::IntoIter: DoubleEndedIterator
 {
-    fn rev(self) -> RevIntoIter<Self> { RevIntoIter { original: self } }
+    /// ```
+    /// use rev::Rev;
+    /// let a = [1, 2, 3];
+    /// let x = a.rev();
+    /// let d: Vec<i32> = x.into_iter().map(|&x| x).collect();
+    /// assert_eq!(d, vec!(3, 2, 1));
+    /// ```
+    fn rev(self) -> RevIntoIter<Self> {
+        RevIntoIter { original: self }
+    }
 }
 
-impl<T> IntoIterator for RevIntoIter<T>
-where
-    T: IntoIterator + Copy,
-    T::IntoIter: DoubleEndedIterator
+impl<T: IntoIterator + Copy> Rev for T
+    where T::IntoIter: DoubleEndedIterator
+{
+}
+
+impl<T: IntoIterator + Copy> IntoIterator for RevIntoIter<T>
+    where T::IntoIter: DoubleEndedIterator
 {
     type Item = T::Item;
     type IntoIter = std::iter::Rev<T::IntoIter>;
@@ -31,31 +34,15 @@ where
     }
 }
 
-impl<T> Copy for RevIntoIter<T>
-where
-    T: IntoIterator + Copy,
-    T::IntoIter: DoubleEndedIterator
+impl<T: IntoIterator + Copy> Copy for RevIntoIter<T>
+    where T::IntoIter: DoubleEndedIterator
 {
 }
 
-impl<T> Clone for RevIntoIter<T>
-where
-    T: IntoIterator + Copy,
-    T::IntoIter: DoubleEndedIterator
+impl<T: IntoIterator + Copy> Clone for RevIntoIter<T>
+    where T::IntoIter: DoubleEndedIterator
 {
     fn clone(&self) -> Self {
         Self { original: self.original }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn test() {
-        let a = [1, 2, 3];
-        let x = a.rev();
-        let d: Vec<i32> = x.into_iter().map(|&x| x).collect();
-        assert_eq!(d, vec!(3, 2, 1));
     }
 }
